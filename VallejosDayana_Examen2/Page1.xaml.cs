0f6f8f524fc6804ede4fd1be40b369499575fdb3
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2024-11-29 07:51:43 -0500

    Creacion Pag 1

--- VallejosDayana_Examen2/Page1.xaml.cs
+++ VallejosDayana_Examen2/Page1.xaml.cs
@@ -1,8 +1,8 @@
 namespace VallejosDayana_Examen2;
 
-public partial class NewPage1 : ContentPage
+public partial class Page1 : ContentPage
 {
-	public NewPage1()
+	public Page1()
 	{
 		InitializeComponent();
 	}
