c961a004c16855aec2b128a9c55893a8bab5085c
DayaPau <dayana.vallejos.casanova@udla.edu.ec>
2024-11-29 08:12:19 -0500

    Manejo de Archivos

--- VallejosDayana_Examen2/Page1.xaml.cs
+++ VallejosDayana_Examen2/Page1.xaml.cs
@@ -11,45 +11,54 @@ public partial class Page1 : ContentPage
     public Page1()
     {
         InitializeComponent();
+
         BindingContext = this;
+
         LoadLastRecarga();
     }
 
+   
     private void OnRecargaClicked(object sender, EventArgs e)
     {
         string name = nameEntry.Text;
         string phone = phoneEntry.Text;
 
+        
         if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(phone))
         {
             DisplayAlert("Error", "Por favor ingrese todos los datos.", "OK");
             return;
         }
 
-        // Guardar
-        string fileName = name.Replace(" ", "") + ".txt";
+       
+        string fileName = name.Replace(" ", "") + ".txt";  
         string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), fileName);
 
+       
         string recargaInfo = $"Nombre: {name}\nTeléfono: {phone}\nFecha: {DateTime.Now}";
-        File.WriteAllText(filePath, recargaInfo);
-
-
-        // Actualizar la última recarga
-        LastRecarga = recargaInfo;
-        OnPropertyChanged(nameof(LastRecarga));
-
-      
-        DisplayAlert("Éxito", "La recarga fue realizada exitosamente.", "OK");
 
         
+        File.WriteAllText(filePath, recargaInfo); //Guardar la recarga en el archivo
+
+       
+        LastRecarga = recargaInfo;
+
+        
+        OnPropertyChanged(nameof(LastRecarga));
+
+        
+        DisplayAlert("Éxito", "La recarga fue realizada exitosamente.", "OK");
+
         LoadLastRecarga();
     }
 
+    // Método para ultima recarga
     private void LoadLastRecarga()
     {
         string fileName = "DayanaVallejos.txt"; 
         string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), fileName);
 
+        
         if (File.Exists(filePath))
         {
             LastRecarga = File.ReadAllText(filePath);
